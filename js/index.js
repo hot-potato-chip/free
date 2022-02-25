0909070a52132737d3e56d84352eb43a681483da
blackpig <15184486929@163.com>
2022-02-26 01:39:32 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const video = document.getElementById('v')
+const canvas = document.getElementById('c')
+
 window.addEventListener('load', () => {
   setup()
   const scanELe = document.getElementById('scan')
@@ -7,10 +10,11 @@ window.addEventListener('load', () => {
   backToContentEle.addEventListener('click', hideCameraContainer)
 })
 
+// 初始化
 function setup() {
-  const video = document.getElementById('v')
   // 判断了浏览器是否支持挂载在MediaDevices.getUserMedia()的方法
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // 获取摄像头模式，默认设置是后置摄像头
     const facingMode = {
       exact: 'environment'
     }
@@ -27,6 +31,12 @@ function setup() {
       } else {
         video.src = stream;
       }
+      // 不希望用户来拖动进度条的话，可以直接使用playsinline属性，webkit-playsinline属性
+      video.playsInline = true;
+      const playPromise = video.play();
+      // playPromise.catch(() => (this.showPlay = true));
+      // 视频开始播放时进行周期性扫码识别
+      playPromise.then(run);
     };
     // 捕获视频流
     navigator.mediaDevices
@@ -48,6 +58,21 @@ function setup() {
       });
   }
 }
+
+function run() {
+  requestAnimationFrame(tick);
+}
+
+// 周期性扫码识别
+function tick() {
+  // 视频处于准备阶段，并且已经加载足够的数据
+  if (video && video.readyState === video.HAVE_ENOUGH_DATA) {
+    // 开始在画布上绘制视频
+    canvas.drawImage(video, 0, 0, canvas.width, canvas.height);
+    run();
+  }
+}
+
 
 function showCameraContainer() {
   setCameraTrans('0%')
